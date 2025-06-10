565bd0d75eaa649758278de492021415e1c5f880
Manfred Steyer <manfred.steyer@gmx.net>
2025-06-10 14:19:27 +0200

    add incremental hydration

--- projects/shell/src/app/app.config.server.ts
+++ projects/shell/src/app/app.config.server.ts
@@ -1,10 +1,13 @@
+import { provideServerRendering } from '@angular/ssr';
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { provideClientHydration, withIncrementalHydration } from '@angular/platform-browser';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+    provideServerRendering(
+    ),
+    provideClientHydration(withIncrementalHydration())
   ]
 };
 
